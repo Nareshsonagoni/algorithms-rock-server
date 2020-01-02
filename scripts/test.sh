36a35a3e002597da902237552a51cc72e13b8a7b
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 20:45:23 +1100

    Testing entrypoint

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
 echo "Running tests on a docker container"
-docker run -v $(pwd):/app -it --rm  node:alpine app/scripts/run_test.sh
+docker run -v $(pwd):/app -it --rm --entrypoint "/bin/sh" node:alpine
