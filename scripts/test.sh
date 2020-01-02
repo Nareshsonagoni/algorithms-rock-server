885a6b6ed8015af7bd5a4ef630f0eb7f81e2de2e
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 21:26:31 +1100

    Testing mount.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,14 @@
 echo "Running tests on a docker container"
 pwd
 
-docker run -v $(pwd):/app \
-  -it \
-  -v "/var/lib/buildkite/builds:/var/lib/buildkite/builds" \
-  -v "/usr/local/bin/buildkite-agent:/usr/local/bin/buildkite-agent" \
-  -v "/var/run/docker.sock:/var/run/docker.sock" \
-  -e "BUILDKITE_BUILD_PATH=/var/lib/buildkite/builds" \
-  --entrypoint "/bin/sh" \
-  -w /app \
-  --rm  node:alpine /scripts/run_test.sh
+docker run -v $(pwd):/app -it --rm --entrypoint  "pwd"  node:alpine
+
+# docker run -v $(pwd):/app \
+#   -it \
+#   -v "/var/lib/buildkite/builds:/var/lib/buildkite/builds" \
+#   -v "/usr/local/bin/buildkite-agent:/usr/local/bin/buildkite-agent" \
+#   -v "/var/run/docker.sock:/var/run/docker.sock" \
+#   -e "BUILDKITE_BUILD_PATH=/var/lib/buildkite/builds" \
+#   --entrypoint "/bin/sh" \
+#   -w /app \
+#   --rm  node:alpine /scripts/run_test.sh
