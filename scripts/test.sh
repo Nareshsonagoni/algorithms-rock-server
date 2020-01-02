c48b344e53bfa19672fa4287a2a1ad3dad383ac1
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 21:07:42 +1100

    Enabling mounting.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,11 @@
 echo "Running tests on a docker container"
 pwd
 
-docker run -v $(pwd):/app -it --rm  node:alpine /buildkite/builds/8d3ff62e811d-1/algorithms-rock-1/algorithms-rock-server/scripts/run_test.sh
+docker run -v $(pwd):/app -it \
+  -v "/var/lib/buildkite/builds:/var/lib/buildkite/builds" \
+  -v "/usr/local/bin/buildkite-agent:/usr/local/bin/buildkite-agent" \
+  -v "/var/run/docker.sock:/var/run/docker.sock" \
+  -e "BUILDKITE_BUILD_PATH=/var/lib/buildkite/builds" \
+  -d \
+  -t \
+  --rm  node:alpine /buildkite/builds/8d3ff62e811d-1/algorithms-rock-1/algorithms-rock-server/scripts/run_test.sh
