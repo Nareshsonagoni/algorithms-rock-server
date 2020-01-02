87a1d15d35dfa5db93b607ffde9244ca4a554b76
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 21:47:54 +1100

    root

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,10 +5,6 @@ pwd
 
 docker run -v $(pwd):/app \
   -it \
-  -v "/var/lib/buildkite/builds:/var/lib/buildkite/builds" \
-  -v "/usr/local/bin/buildkite-agent:/usr/local/bin/buildkite-agent" \
-  -v "/var/run/docker.sock:/var/run/docker.sock" \
-  -e "BUILDKITE_BUILD_PATH=/var/lib/buildkite/builds" \
   --entrypoint "ls" \
-  -w /usr/local/bin/buildkite-agent \
+  -w / \
   --rm  node:alpine
