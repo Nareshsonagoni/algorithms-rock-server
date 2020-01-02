97cc161d5fa8e5f9580a480b5fb56705584d3a2f
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 20:51:39 +1100

    Test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 echo "Running tests on a docker container"
 pwd
 
-docker run -v $(pwd):/app -it --rm  node:alpine scripts/run_test.sh
+docker run -v $(pwd):/app -it --rm  node:alpine /buildkite/builds/8d3ff62e811d-1/algorithms-rock-1/algorithms-rock-server/scripts/run_test.sh
