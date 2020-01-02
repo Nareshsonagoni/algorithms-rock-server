6b855f7b61b73588bdaa6907e094cf79c16d44f3
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 22:49:25 +1100

    Minor fix.

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,4 +7,4 @@ docker run -v /Users/kkisha/Desktop/Projects/algorithms-rock-server:/app \
   -it \
   --entrypoint "/bin/sh" \
   -w /app \
-  --rm  node:alpine /scripts/run_test.sh
+  --rm  node:alpine scripts/run_test.sh
