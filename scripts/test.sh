7e191940e1584ada5ff9d212ad50bfc994f7f3d6
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 22:08:28 +1100

    Mounting from host.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@ pwd
 # ls
 # docker run -v $(pwd):/app -it --rm --entrypoint  "ls"  node:alpine
 
-docker run -v /buildkite/builds/7cd9ce752449-1/algorithms-rock-1/algorithms-rock-server:/app \
+docker run -v /Users/kkisha/Desktop/Projects/algorithms-rock-server:/app \
   -it \
   -v $(pwd):/app \
   --entrypoint "ls" \
