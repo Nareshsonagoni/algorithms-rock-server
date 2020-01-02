52206c7c197e0a22a2aaf90eb4545e70fecb4876
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 21:11:19 +1100

    Removing -it.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,11 @@
 echo "Running tests on a docker container"
 pwd
 
-docker run -v $(pwd):/app -it \
+docker run -v $(pwd):/app \
   -v "/var/lib/buildkite/builds:/var/lib/buildkite/builds" \
   -v "/usr/local/bin/buildkite-agent:/usr/local/bin/buildkite-agent" \
   -v "/var/run/docker.sock:/var/run/docker.sock" \
   -e "BUILDKITE_BUILD_PATH=/var/lib/buildkite/builds" \
+  -d \
   -t \
   --rm  node:alpine /app/scripts/run_test.sh
