b8daed6c3983dfae0ffcaa3aad796c9477413f86
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 21:15:06 +1100

    Mounting onto root.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,10 @@
 echo "Running tests on a docker container"
 pwd
 
-docker run -v $(pwd):/app \
+docker run -v $(pwd):/ \
+  -it \
   -v "/var/lib/buildkite/builds:/var/lib/buildkite/builds" \
   -v "/usr/local/bin/buildkite-agent:/usr/local/bin/buildkite-agent" \
   -v "/var/run/docker.sock:/var/run/docker.sock" \
   -e "BUILDKITE_BUILD_PATH=/var/lib/buildkite/builds" \
-  -it \
-  --rm  node:alpine /app/scripts/run_test.sh
+  --rm  node:alpine /scripts/run_test.sh
