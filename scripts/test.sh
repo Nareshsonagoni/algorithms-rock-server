44ae03745d37831905ede1e099aa3e04fd420c4f
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 20:49:10 +1100

    Modifying path.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,3 @@
 echo "Running tests on a docker container"
-docker run -v $(pwd):/app -it --rm --entrypoint "/bin/sh" node:alpine
+
+docker run -v $(pwd):/app -it --rm  node:alpine scripts/run_test.sh
