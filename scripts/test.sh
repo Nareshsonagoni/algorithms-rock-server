a35040fed06e900c7a967ec787601efc95762221
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 22:04:15 +1100

    trying app

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,11 @@
 echo "Running tests on a docker container"
 pwd
-ls
+# ls
 # docker run -v $(pwd):/app -it --rm --entrypoint  "ls"  node:alpine
 
-docker run -v /buildkite/builds/7cd9ce752449-1/algorithms-rock-1/algorithms-rock-server:/buildkite/builds/7cd9ce752449-1/algorithms-rock-1/algorithms-rock-server \
+docker run -v /buildkite/builds/7cd9ce752449-1/algorithms-rock-1/algorithms-rock-server:/app \
   -it \
   -v $(pwd):/app \
   --entrypoint "ls" \
-  -w /buildkite/builds/7cd9ce752449-1/algorithms-rock-1/algorithms-rock-server \
+  -w /app \
   --rm  node:alpine
