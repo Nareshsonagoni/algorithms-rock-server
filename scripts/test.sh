52d8226901a70509c0f3e5babc6a8f3049398d07
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-02 21:10:18 +1100

    Test.

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,5 @@ docker run -v $(pwd):/app -it \
   -v "/usr/local/bin/buildkite-agent:/usr/local/bin/buildkite-agent" \
   -v "/var/run/docker.sock:/var/run/docker.sock" \
   -e "BUILDKITE_BUILD_PATH=/var/lib/buildkite/builds" \
+  -t \
   --rm  node:alpine /app/scripts/run_test.sh
