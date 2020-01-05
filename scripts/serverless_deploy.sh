d0025617ae1fa533ffaff3826b0b51c3630ebea6
Naresh Sonagoni <nareshsonagoni@gmail.com>
2020-01-05 21:04:58 +1100

    fixing aws login

--- scripts/serverless_deploy.sh
+++ scripts/serverless_deploy.sh
@@ -1,2 +1,4 @@
 
+serverless config credentials --provider aws --key AKIA55QE67I5I2U2G4VY --secret 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
+# Don't follow this method(providing credentials is a bad practice)
 serverless deploy -v
