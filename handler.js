5da10b848dc668e725ad744364f884151726a84a
Naresh <nareshsonagoni@gmail.com>
2020-02-27 14:36:31 +1100

    fixed a binarySearch func

--- handler.js
+++ handler.js
@@ -1,17 +1,10 @@
-'use strict';
-const binary = require('./algorithms/search/binary');
+"use strict";
+const binary = require("./algorithms/search/binary");
 
 module.exports.binarySearch = async event => {
-
   const result = binary(parseInt(event.pathParameters.item, 10));
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        result: `Index of the number ${result + ''}`,
-      },
-      null,
-      2,
-    ),
+    body: JSON.stringify(result, null, 2)
   };
 };
