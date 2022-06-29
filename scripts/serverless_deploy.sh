9fb4ddcf23e9beed0a271f55da1388ad0746bdb4
Naresh Sonagoni <33779671+nash4j@users.noreply.github.com>
2022-06-29 12:55:02 +1000

    Update serverless_deploy.sh

--- scripts/serverless_deploy.sh
+++ scripts/serverless_deploy.sh
@@ -1,4 +1,4 @@
 
-serverless config credentials --provider aws --key AKIA55QE67I5I2U2G4VY --secret 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
+serverless config credentials --provider aws --key "key" --secret "secret_key"
 # Don't follow this method(providing credentials is a bad practice)
 serverless deploy -v
